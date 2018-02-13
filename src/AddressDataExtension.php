5bec2835d383709446b7eb8dadcbf0ed46d5d09a
mak001 <mak001001@gmail.com>
2018-02-13 15:41:38 -0600

    Added method to get a static map Resolves #17

--- src/AddressDataExtension.php
+++ src/AddressDataExtension.php
@@ -14,12 +14,12 @@ class AddressDataExtension extends DataExtension
      * @var array
      */
     private static $db = [
-        'Address'  => 'Varchar(255)',
+        'Address' => 'Varchar(255)',
         'Address2' => 'Varchar(255)',
-        'City'   => 'Varchar(64)',
-        'State'    => 'Varchar(64)',
+        'City' => 'Varchar(64)',
+        'State' => 'Varchar(64)',
         'PostalCode' => 'Varchar(10)',
-        'Country'  => 'Varchar(2)',
+        'Country' => 'Varchar(2)',
         'Lat' => 'Decimal(10,7)',
         'Lng' => 'Decimal(10,7)',
     ];
@@ -29,7 +29,7 @@ class AddressDataExtension extends DataExtension
      */
     public function updateCMSFields(FieldList $fields)
     {
-        $tab_name = Config::inst()->get(AddressDataExtension::class,'address_tab_name');
+        $tab_name = Config::inst()->get(AddressDataExtension::class, 'address_tab_name');
 
         $fields->addFieldsToTab('Root.' . $tab_name, [
             TextField::create('Address'),
@@ -39,7 +39,7 @@ class AddressDataExtension extends DataExtension
             TextField::create('PostalCode'),
             TextField::create('Country'),
             ReadonlyField::create('Lat'),
-            ReadonlyField::create('Lng')
+            ReadonlyField::create('Lng'),
         ]);
     }
 
@@ -48,22 +48,25 @@ class AddressDataExtension extends DataExtension
      *
      * @return string
      */
-    public function getFullAddress() {
-        $parts = array(
+    public function getFullAddress()
+    {
+        $parts = [
             $this->owner->Address,
             $this->owner->Address2,
             $this->owner->City,
             $this->owner->State,
             $this->owner->PostalCode,
             $this->owner->Country,
-        );
+        ];
+
         return implode(', ', array_filter($parts));
     }
 
     /**
      * @return bool
      */
-    public function hasAddress() {
+    public function hasAddress()
+    {
         return (
             $this->owner->Address
             && $this->owner->City
@@ -71,20 +74,49 @@ class AddressDataExtension extends DataExtension
             && $this->owner->PostalCode
         );
     }
+
+    /**
+     * Returns a static google map of the address, linking out to the address.
+     *
+     * @param int $width (optional)
+     * @param int $height (optional)
+     * @param int $scale (optional)
+     *
+     * @return string
+     */
+    public function AddressMap($width = 320, $height = 240, $scale = 1)
+    {
+        $data = $this->owner->customise([
+            'Width' => $width,
+            'Height' => $height,
+            'Scale' => $scale,
+            'Address' => rawurlencode($this->getFullAddress()),
+            'Key' => Config::inst()->get(GoogleGeocoder::class, 'geocoder_api_key'),
+        ]);
+
+        return $data->renderWith('Dynamic/Geocoder/AddressMap');
+    }
+
     /**
      * Returns TRUE if any of the address fields have changed.
      *
      * @param int $level
+     *
      * @return bool
      */
-    public function isAddressChanged($level = 1) {
-        $fields  = array('Address', 'Address2', 'City', 'State', 'PostalCode', 'Country');
+    public function isAddressChanged($level = 1)
+    {
+        $fields = ['Address', 'Address2', 'City', 'State', 'PostalCode', 'Country'];
         $changed = $this->owner->getChangedFields(false, $level);
         foreach ($fields as $field) {
-            if (array_key_exists($field, $changed)) return true;
+            if (array_key_exists($field, $changed)) {
+                return true;
+            }
         }
+
         return false;
     }
+
     /**
      *
      */
---
+++ templates/Dynamic/Geocoder/AddressMap.ss
@@ -0,0 +1,5 @@
+<div class="addressMap">
+    <a href="//maps.google.com/?q=$Address">
+        <img src="//maps.googleapis.com/maps/api/staticmap?size={$Width}x{$Height}&scale={$Scale}&maptype={$MapType}&markers=$Address&key=$Key" alt="$FullAddress.ATT" />
+    </a>
+</div>
